254e8c859c0b60d2f3f3e20ebe3a9976effb5def
RyanROCKET-YYH <116342145+RyanROCKET-YYH@users.noreply.github.com>
2024-02-14 17:11:27 -0500

    Add GPIO initialization for pin A8

--- src/main.c
+++ src/main.c
@@ -10,6 +10,10 @@
 int main() {
   uart_polling_init(115200);
   printk("hello world");
+
+  gpio_init(GPIO_A, 8, MODE_GP_OUTPUT, OUTPUT_PUSH_PULL, OUTPUT_SPEED_LOW, PUPD_NONE, ALT0);
+  gpio_set(GPIO_A, 8);
+
   while(1) {
     char test = uart_polling_get_byte();
     uart_polling_put_byte(test);
