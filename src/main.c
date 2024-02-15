42215d10ae7ac1ecb07d79dccd4cee1eb30bd439
Yuhong Yao <yuhongy@andrew.cmu.edu>
2024-02-15 07:47:06 -0800

    LED and BUTTON tested

--- src/main.c
+++ src/main.c
@@ -6,17 +6,53 @@
 #include <lcd_driver.h>
 #include <keypad_driver.h>
 
+#define YELLOW_LED_PORT (0)
+#define YELLOW_LED_PIN  (8)
+#define BLUE_LED_PORT   (0)
+#define BLUE_LED_PIN    (9)
+
+#define BUTTON1_PORT (2)		// PC_7
+#define BUTTON1_PIN  (7)
+#define BUTTON2_PORT (1)		// PB_6
+#define BUTTON2_PIN  (6)
+
+
+void lazy_delay(unsigned int milliseconds) {
+  volatile unsigned int i, j;
+  for (i = 0; i < milliseconds; i++) {
+    for (j = 0; j < 10000; j++) {
+      __asm("NOP");
+    }
+  }
+}
 
 int main() {
   uart_polling_init(115200);
-  printk("hello world");
+  //printk("hello world");
 
-  gpio_init(GPIO_A, 8, MODE_GP_OUTPUT, OUTPUT_PUSH_PULL, OUTPUT_SPEED_LOW, PUPD_NONE, ALT0);
-  gpio_set(GPIO_A, 8);
+  gpio_init(YELLOW_LED_PORT, YELLOW_LED_PIN, MODE_GP_OUTPUT, OUTPUT_PUSH_PULL, OUTPUT_SPEED_LOW, PUPD_NONE, ALT0);
+  gpio_init(BLUE_LED_PORT, BLUE_LED_PIN, MODE_GP_OUTPUT, OUTPUT_PUSH_PULL, OUTPUT_SPEED_LOW, PUPD_NONE, ALT0);
+
+  gpio_init(BUTTON1_PORT, BUTTON1_PIN, MODE_INPUT, OUTPUT_PUSH_PULL, OUTPUT_SPEED_LOW, PUPD_PULL_DOWN, ALT0);
 
   while(1) {
-    char test = uart_polling_get_byte();
-    uart_polling_put_byte(test);
+    // Turn on the LEDs
+ /* gpio_set(YELLOW_LED_PORT, YELLOW_LED_PIN);
+    gpio_set(BLUE_LED_PORT, BLUE_LED_PIN);
+    printk("LEDs ON\n");
+    lazy_delay(1000); // 1 second delay
+    // Turn off the LEDs
+    gpio_clr(YELLOW_LED_PORT, YELLOW_LED_PIN);
+    gpio_clr(BLUE_LED_PORT, BLUE_LED_PIN);
+    printk("LEDs OFF\n");
+    lazy_delay(1000); // 1 second delay
+ */
+    
+    int button1_state = gpio_read(BUTTON1_PORT, BUTTON1_PIN);
+    printk("BUTTON1 State:%d\n", button1_state);
+
+    //char test = uart_polling_get_byte();
+    //uart_polling_put_byte(test);
   }
 
   return 0;
