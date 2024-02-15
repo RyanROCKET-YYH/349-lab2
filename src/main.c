3efc785ab8575192754a7e66d096b53c941819b4
yiyingl4 <123005953+yiyingl4@users.noreply.github.com>
2024-02-14 19:23:31 -0500

    Update "Test: button in"

--- src/main.c
+++ src/main.c
@@ -10,10 +10,26 @@
 int main() {
   uart_polling_init(115200);
   printk("hello world");
-  while(1) {
-    char test = uart_polling_get_byte();
-    uart_polling_put_byte(test);
-  }
+
+  // Test: one LED
+  //gpio_init(GPIO_A, 8, MODE_GP_OUTPUT, OUTPUT_PUSH_PULL, OUTPUT_SPEED_LOW, PUPD_PULL_UP, ALT0);
+  //gpio_set(GPIO_A, 8);
+
+  // Test: button input
+  // gpio_init(GPIO_A, 8, MODE_GP_OUTPUT, OUTPUT_PUSH_PULL, OUTPUT_SPEED_LOW, PUPD_PULL_UP, ALT0); 
+  // gpio_set(GPIO_A, 8); //set PA_A into high
+  // gpio_init(GPIO_A, 9, MODE_INPUT, OUTPUT_PUSH_PULL, OUTPUT_SPEED_LOW, PUPD_PULL_DOWN, ALT0); // Read the input
+  // while(1){
+  //   int read_value = gpio_read(GPIO_A, 9);
+  //   printk("%d\n",read_value);
+  // }
+  
+  // 
+  
+  // while(1) {
+  //   char test = uart_polling_get_byte();
+  //   uart_polling_put_byte(test);
+  // }
 
   return 0;
 }
