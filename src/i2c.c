d76f9a8a12215b114faa6317dcae5abfe3f7b85b
Yuhong Yao <yuhongy@andrew.cmu.edu>
2024-02-16 16:04:29 -0500

    i2c map

--- src/i2c.c
+++ src/i2c.c
@@ -3,6 +3,25 @@
 #include <unistd.h>
 #include <rcc.h>
 
+// D14 I2C1_SDA & D15 I2C1_SCL
+/** @brief The UART register map. */
+struct i2c_reg_map {
+    volatile uint32_t CR1;      /**<  Control Register 1 */
+    volatile uint32_t CR2;      /**<  Control Register 2 */
+    volatile uint32_t OAR1;     /**<  Own Address Register 1 */
+    volatile uint32_t OAR2;     /**<  Own Address Register 2 */
+    volatile uint32_t DR;       /**<  Data Register */
+    volatile uint32_t SR1;      /**<  Status Register 1 */
+    volatile uint32_t SR2;      /**<  Status Register 2 */
+    volatile uint32_t CCR;      /**<  Clock Control Register */
+    volatile uint32_t TRISE;    /**<  TRISE Register */
+    volatile uint32_t FLTR;     /**<  FLTR Register */
+};
+
+/** @brief Base Address of I2C1 */
+#define I2C1_BASE   (struct i2c_reg_map *) 0x40005400
+
+#define I2C_EN  (1 << )
 void i2c_master_init(uint16_t clk){
     (void) clk; /* This line is simply here to suppress the Unused Variable Error. */
                 /* You should remove this line in your final implementation */
