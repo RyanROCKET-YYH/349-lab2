c450cc2e7a76ebabdbff89f9f533a004f2928a21
Yuhong Yao <yuhongy@andrew.cmu.edu>
2024-02-23 13:15:55 -0500

    test

--- src/lcd_driver.c
+++ src/lcd_driver.c
@@ -6,149 +6,64 @@
 #define I2C_SLAVE_ADDR_W 0x4E   // 7 bit address
 #define I2C_SLAVE_ADDR_R 0x4F
 
-void lcd_driver_init(){
+void lcd_send_instruction(uint8_t command) {
     uint8_t LCD_ADDR = I2C_SLAVE_ADDR_W; 
     uint8_t i2c_write_buf[4];
-    // Prepare the upper half : 0b0011 with control bits being 1100 (1=1, E = 1, RW=0, RS=0)
-    // RS is set low since it's an instruction
-    i2c_write_buf[0] = 0b0011 << 4 | 0b1100;
-    i2c_write_buf[1] = 0b0011 << 4 | 0b1000;
-    i2c_write_buf[2] = 0b0000 << 4 | 0b1100;
-    i2c_write_buf[3] = 0b0000 << 4 | 0b1000;
-    i2c_master_write(i2c_write_buf, 4, LCD_ADDR);   // Function set
-    i2c_master_write(i2c_write_buf, 4, LCD_ADDR);   // Function set
-    i2c_master_write(i2c_write_buf, 4, LCD_ADDR);   // Function set
-   
-    i2c_write_buf[0] = 0b0010 << 4 | 0b1100;
-    i2c_write_buf[1] = 0b0010 << 4 | 0b1000;
-    i2c_master_write(i2c_write_buf, 4, LCD_ADDR);   // Function set (set interface to 4 bits long)
+    // (1=1, E = 1, RW=0, RS=0)
+    i2c_write_buf[0] = (command & 0xF0) | 0b1100;
+    i2c_write_buf[1] = (command & 0xF0) | 0b1000;
+    i2c_write_buf[2] = command << 4 | 0b1100;
+    i2c_write_buf[3] = command << 4 | 0b1000;
+    i2c_master_write(i2c_write_buf, 4, LCD_ADDR);
+}
+
+void lcd_send_data(uint8_t data) {
+    uint8_t LCD_ADDR = I2C_SLAVE_ADDR_W; 
+    uint8_t i2c_write_buf[4];
+    // (1=1, E = 1, RW=0, RS=1)
+    i2c_write_buf[0] = (data & 0xF0) | 0b1101;
+    i2c_write_buf[1] = (data & 0xF0) | 0b1001;
+    i2c_write_buf[2] = data << 4 | 0b1101;
+    i2c_write_buf[3] = data << 4 | 0b1001;
+    i2c_master_write(i2c_write_buf, 4, LCD_ADDR);
+}
+
+void lcd_driver_init(){
+    lcd_send_instruction(0b00110000);
+    lcd_send_instruction(0b00110000);
+    lcd_send_instruction(0b00110000);
+
+    lcd_send_instruction(0b00100000);  // Function set (set interface to 4 bits long)
    
     // clear display
-    i2c_write_buf[0] = 0b0000 << 4 | 0b1100;
-    i2c_write_buf[1] = 0b0000 << 4 | 0b1000;
-    i2c_write_buf[2] = 0b0001 << 4 | 0b1100;
-    i2c_write_buf[3] = 0b0001 << 4 | 0b1000;
-    i2c_master_write(i2c_write_buf, 4, LCD_ADDR);
-
+    lcd_send_instruction(0b00000001);
     for(int i = 1; i< 2000000; i++){}
-    // Display ON/OFF Control: turn display on (D=1), cursor on (C=1), and blinking on (B=1)
-    i2c_write_buf[0] = 0b0000 << 4 | 0b1100; // Prepare instruction with control bits
-    i2c_write_buf[1] = 0b0000 << 4 | 0b1000; // Same as above, making sure P3=1 if using backlight
-    i2c_write_buf[2] = 0b1111 << 4 | 0b1100; // Set D, C, and B bits to 1
-    i2c_write_buf[3] = 0b1111 << 4 | 0b1000; // Execute instruction with control bits
-    i2c_master_write(i2c_write_buf, 4, LCD_ADDR);
 }
-
-uint8_t decide_upper(char input){
-    if(input == (' ' || '!' || ',')){
-        return 0b0010;
-    }
-    else if(input == ('I' || 'L')){
-        return 0b0100;
-    }
-    else if(input == 'U'){
-        return 0b0101;
-    }
-    else if(input == ('a' || 'c' || 'd' || 'e' || 'g' || 'i' || 'k' || 'l' || 'n' || 'o')){
-        return 0b0110;
-    }
-    else if( input == ('p' || 'r' || 's' || 't' || 'y') ){
-        return 0b0111;
-    }
-    // default case: ?
-    else{
-        // ?
-        return 0b0011;
-    }
-}
-
-uint8_t decide_lower(char input){
-    if(input == (' ' || 'p')){
-        return 0b0000;
-    }
-    else if(input == ('!' || 'a')){
-        return 0b0001;
-    }
-    else if(input == ('r')){
-        return 0b0010;
-    }
-    else if(input == ('c' || 's')){
-        return 0b0011;
-    }
-    else if(input == ('d' || 't')){
-        return 0b0100;
-    }
-    else if(input == ('U' || 'e')){
-        return 0b0101;
-    }
-    else if(input == 'g'){
-        return 0b0111;
-    }
-    else if(input == ('I' || 'i' || 'y')){
-        return 0b1001;
-    }
-    else if(input == 'k'){
-        return 0b1011;
-    }
-    else if(input == (',' || 'L' || 'l')){
-        return 0b1100;
-    }
-    else if(input == 'n'){
-        return 0b1110;
-    }
-    else if(input == 'o'){
-        return 0b1111;
-    }
-    // default case: ?
-    else{
-        // ?
-        return 0b1111;
-    }
-}
-
 
 void lcd_print(char *input){
-    (void) input;
-    uint8_t LCD_ADDR = I2C_SLAVE_ADDR_W; //TODO:
-    uint8_t upper_bits;
-    uint8_t lower_bits;
-    uint8_t i2c_write_buf[4];
-
-    // to get the upper bits and lower bits of the input character
-    for (int i = 0; input[i] != '\0'; ++i) {
-        upper_bits = decide_upper(input[i]);
-        lower_bits = decide_lower(input[i]);
-        // set the buffer
-        i2c_write_buf[0] = upper_bits << 4 | 0b1101;
-        i2c_write_buf[1] = upper_bits << 4 | 0b1001;
-        i2c_write_buf[2] = lower_bits << 4 | 0b1101;
-        i2c_write_buf[3] = lower_bits << 4 | 0b1001;
-        // write
-        i2c_master_write(i2c_write_buf, 4, LCD_ADDR);
+    while (*input) {
+        lcd_send_data((uint8_t)(*input));
+        input++;
     }
-    return;
-    
 }
 
 void lcd_set_cursor(uint8_t row, uint8_t col){
-    (void) row;
-    (void) col;
-    // uint8_t addr;
-    
+    uint8_t address;
+    switch(row) {
+        case 0:
+            address = 0x00 + col;
+            break;
+        case 1:
+            address = 0x40 + col;
+            break;
+        default:
+            address = 0x00;
+    }
+    lcd_send_instruction(0x80 | address);
 }
 
 void lcd_clear(){
-    uint8_t LCD_ADDR = I2C_SLAVE_ADDR_W;    //0b0100111 R/W
-    uint8_t i2c_write_buf[4];
-    
-    // clear display
-    i2c_write_buf[0] = 0b0000 << 4 | 0b1100;
-    i2c_write_buf[1] = 0b0000 << 4 | 0b1000;
-    i2c_write_buf[2] = 0b0001 << 4 | 0b1100;
-    i2c_write_buf[3] = 0b0001 << 4 | 0b1000;
-    i2c_master_write(i2c_write_buf, 4, LCD_ADDR);
-
-    // wait for 2 second
+    lcd_send_instruction(0b00000001);
     for(int i = 1; i< 2000000; i++){}
     return;
 }
--- src/main.c
+++ src/main.c
@@ -6,16 +6,9 @@
 #include <lcd_driver.h>
 #include <keypad_driver.h>
 
-#define YELLOW_LED_PORT (0)
-#define YELLOW_LED_PIN  (8)
-#define BLUE_LED_PORT   (0)
-#define BLUE_LED_PIN    (9)
 
-#define BUTTON1_PORT (2)		// PC_7
-#define BUTTON1_PIN  (7)
-#define BUTTON2_PORT (1)		// PB_6
-#define BUTTON2_PIN  (6)
-
+#define CORRECT_PASSCODE '#349'
+#define PASSCODE_LENGTH   (4)
 
 void lazy_delay(unsigned int milliseconds) {
   volatile unsigned int i, j;
@@ -26,42 +19,81 @@ void lazy_delay(unsigned int milliseconds) {
   }
 }
 
+int strcmp(const char *s1, const char *s2) {
+  while (*s1 && (*s1 == *s2)) {
+    s1++;
+    s2++;
+  }
+  return *(const unsigned char*)s1 - *(const unsigned char*)s2;
+
+}
+
 int main() {
-  // uart_polling_init(115200);
+  uart_polling_init(115200);
+  keypad_init();
   //printk("hello world");
-
-  //gpio_init(YELLOW_LED_PORT, YELLOW_LED_PIN, MODE_GP_OUTPUT, OUTPUT_PUSH_PULL, OUTPUT_SPEED_LOW, PUPD_NONE, ALT0);
-  //gpio_init(BLUE_LED_PORT, BLUE_LED_PIN, MODE_GP_OUTPUT, OUTPUT_PUSH_PULL, OUTPUT_SPEED_LOW, PUPD_NONE, ALT0);
-
-  //gpio_init(BUTTON1_PORT, BUTTON1_PIN, MODE_INPUT, OUTPUT_PUSH_PULL, OUTPUT_SPEED_LOW, PUPD_PULL_DOWN, ALT0);
+  // BLUE LED (D2)
+  gpio_init(GPIO_A, 10, MODE_GP_OUTPUT, OUTPUT_PUSH_PULL, OUTPUT_SPEED_LOW, PUPD_NONE, ALT0);
+  // RED LED (D4)
+  gpio_init(GPIO_B, 5, MODE_GP_OUTPUT, OUTPUT_PUSH_PULL, OUTPUT_SPEED_LOW, PUPD_NONE, ALT0);
+  // "ENTER" BUTTON (D6)
+  gpio_init(GPIO_B, 10, MODE_INPUT, OUTPUT_PUSH_PULL, OUTPUT_SPEED_LOW, PUPD_PULL_UP, ALT0);
+  // "LOCK" BUTTON (B1)
+  gpio_init(GPIO_C, 13, MODE_INPUT, OUTPUT_PUSH_PULL, OUTPUT_SPEED_LOW, PUPD_PULL_UP, ALT0);
   // keypad_init();
   i2c_master_init(80);
   lcd_driver_init();
-  
-  // printk("LCD CLR");
-  // while(1) {
-//     // Turn on the LEDs
-//  /* gpio_set(YELLOW_LED_PORT, YELLOW_LED_PIN);
-//     gpio_set(BLUE_LED_PORT, BLUE_LED_PIN);
-//     printk("LEDs ON\n");
-//     lazy_delay(1000); // 1 second delay
-//     // Turn off the LEDs
-//     gpio_clr(YELLOW_LED_PORT, YELLOW_LED_PIN);
-//     gpio_clr(BLUE_LED_PORT, BLUE_LED_PIN);
-//     printk("LEDs OFF\n");
-//     lazy_delay(1000); // 1 second delay
-//  */
-    
-//     //int button1_state = gpio_read(BUTTON1_PORT, BUTTON1_PIN);
-//     //printk("BUTTON1 State:%d\n", button1_state);
+  lcd_clear();
+  printk("Enter 'Start' to begin:\n");
+  uint8_t start = 0;
+  while(!start) {
+    char input[5] = {0}; // buffer for start
+    for (int i = 0; i < 5; i++) {
+      input[i] = uart_polling_get_byte();
+      uart_polling_put_byte(input[i]);
+    }
+    input[5] = '\0';
+    if (strcmp(input, "Start") == 0) {
+      start = 1;
+      printk("\n What's your password?\n");
+    } else {
+      start = 0;
+      printk("\n Enter 'Start' to begin:\n");
+    }
+  }
 
-//     //char test = uart_polling_get_byte();
-//     //uart_polling_put_byte(test);
+  char passcode[PASSCODE_LENGTH] = {0};
+  uint8_t index = 0;
+  uint8_t is_locked = 1;
+  while(1) {
+    gpio_set(GPIO_B, 5);
+    char key = keypad_read();
+    if (key != '\0') {
+      char key_pressed[2] = {key, '\0'};
+      lcd_print(key_pressed);
+    }
     
-//     char key = keypad_read();
-//     printk("%c pressed\n", key);
-//     lazy_delay(10);
+    // Turn on the LEDs
+    // gpio_set(GPIO_A, 10);
+    // gpio_set(GPIO_B, 5);
+    // printk("LEDs ON\n");
+    // lazy_delay(1000); // 1 second delay
+    // // Turn off the LEDs
+    // gpio_clr(GPIO_A, 10);
+    // gpio_clr(GPIO_B, 5);
+    // printk("LEDs OFF\n");
+    // lazy_delay(1000); // 1 second delay
+
+    
+    // int button1_state = gpio_read(GPIO_B, 10);
+    // int button2_state = gpio_read(GPIO_C, 13);
+    // printk("BUTTON1 State:%d\n", button1_state);  // pull up too
+    // printk("BUTTON2 State:%d\n", button2_state); // 1 when not pressed, 0 pressed pull up
+    // lazy_delay(100);
+    // char input = uart_polling_get_byte();
+//     uart_polling_put_byte(input);
+    
    
-  // }
+  }
   return 0;
 }
